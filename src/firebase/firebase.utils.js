8cee60765de0ffdf88e31a6aef722b0ef6145d69
shn2002 <joe.shanhenian@gmail.com>
2022-11-05 23:32:49 -0400

    Update firebase.utils.js
    delete firebase api key

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const config = {
-    apiKey: "AIzaSyADLfs1U83SXKbllGxtAEwiOcJSpnv5a80",
-    authDomain: "crwn-db-24f67.firebaseapp.com",
-    projectId: "crwn-db-24f67",
-    storageBucket: "crwn-db-24f67.appspot.com",
-    messagingSenderId: "1043009506161",
-    appId: "1:1043009506161:web:9d2a870da3f096476944d0",
-    measurementId: "G-S0VE9V76SZ"
+
 };
 
 firebase.initializeApp(config);
